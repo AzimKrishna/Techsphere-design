75cedd597bec7c8d2d6b8dd3689bb0301d18ce70
Azim Baji Krishna <azimbaji19@gmail.com>
2023-12-03 12:39:59 +0530

    Added marquee scroll for offerings on small devices

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
   $(".project-card").hover(
     function () {
-      // Mouse over
       $(this).find(".read-more").addClass("expanded");
       $(this).find(".arrow-up").addClass("rotated");
     },
     function () {
-      // Mouse out
       $(this).find(".read-more").removeClass("expanded");
       $(this).find(".arrow-up").removeClass("rotated");
     }
@@ -20,10 +18,8 @@ $(document).ready(function () {
     "img/ara.svg",
   ];
 
-  // Reference to the client-logos container
   const clientLogosContainer = $(".client-logos");
 
-  // Generate logo-holder elements and append them to the container
   for (let i = 0; i < logoUrls.length; i++) {
     const logoHolder = $("<div>").addClass("logo-holder");
     const logoImg = $("<img>")
@@ -35,39 +31,31 @@ $(document).ready(function () {
   }
 
   // Burger click event
-  $('.burger').click(function () {
-    // Toggle the 'v-class' id on the navbar
-    $('.nav-links').attr('id', function (_, attr) {
-      return attr === 'v-class' ? '' : 'v-class';
+  $(".burger").click(function () {
+    $("nav .nav-links").attr("id", function (_, attr) {
+      return attr === "v-class" ? "" : "v-class";
     });
 
-    // Toggle the 'cross' class on the burger
-    $('.burger').toggleClass('cross');
+    $(".burger").toggleClass("cross");
   });
 
   // Nav links click event
-  $('nav .nav-links a').click(function () {
-    // Remove the 'selected' class from all links
-    $('nav .nav-links a').removeClass('selected');
+  $("nav .nav-links a").click(function () {
+    $("nav .nav-links a").removeClass("selected");
 
-    // Add the 'selected' class to the clicked link
-    $(this).addClass('selected');
+    $(this).addClass("selected");
 
-    // Close the side menu by removing the 'v-class' id
-    $('.nav-links').removeAttr('id');
+    $(".nav-links").removeAttr("id");
 
-    // Restore the burger icon to its original state
-    $('.burger').removeClass('cross');
+    $(".burger").removeClass("cross");
   });
 
   // Cache the navigation links
   var navLinks = $("nav .nav-links a");
 
-  // Function to update the selected class based on scroll position
   function updateSelected() {
     var scrollPosition = $(window).scrollTop();
 
-    // Iterate through sections and update the selected class
     $("section").each(function () {
       var section = $(this);
       var sectionId = section.attr("id");
@@ -76,18 +64,43 @@ $(document).ready(function () {
         scrollPosition >= section.offset().top &&
         scrollPosition < section.offset().top + section.outerHeight()
       ) {
-        // Remove selected class from all links
         navLinks.removeClass("selected");
 
-        // Add selected class to the corresponding link
         $('nav  .nav-links a[href="#' + sectionId + '"]').addClass("selected");
       }
     });
   }
 
-  // Call the updateSelected function on document ready and scroll
   $(document).on("scroll", updateSelected);
   updateSelected();
+
+  var navHeight = $("nav").outerHeight();
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    var targetId = $(this).attr("href");
+    var offset = $(targetId).offset().top - navHeight;
+
+    $("html, body").animate(
+      {
+        scrollTop: offset,
+      },
+      100
+    );
+  }
+
+  // Attach or detach click event based on window width
+  function updateSmoothScroll() {
+    if ($(window).width() < 800) {
+      $(".nav-links a").on("click", smoothScroll);
+    } else {
+      $(".nav-links a").off("click", smoothScroll);
+    }
+  }
+
+  updateSmoothScroll();
+
+  $(window).resize(updateSmoothScroll);
 });
 
 for (let i = 1; i <= 4; i++) {
@@ -95,17 +108,14 @@ for (let i = 1; i <= 4; i++) {
     url: "https://randomuser.me/api/",
     dataType: "json",
     success: function (data) {
-      // Extract user data from the API response
       const user = data.results[0];
 
-      // Select the card corresponding to the current iteration
       const card = $(`.testimonials-cards-holder .cards:nth-child(${i})`);
       const pfpHolder = card.find(".pfp-holder img");
       const nameElement = card.find(".name");
       const designationElement = card.find(".designation");
       const reviewElement = card.find(".review");
 
-      // Fill in the content of the HTML elements with user data
       pfpHolder.attr("src", user.picture.large);
       nameElement.text(
         `${user.name.title} ${user.name.first} ${user.name.last}`
@@ -117,18 +127,20 @@ for (let i = 1; i <= 4; i++) {
     },
   });
 }
-    $(document).ready(function () {
-        var navHeight = $('nav').outerHeight();
-      // Smooth scroll to target section with an offset
-      $('.nav-links a').on('click', function (event) {
-        event.preventDefault();
-        var targetId = $(this).attr('href');
-        var offset = $(targetId).offset().top - navHeight;
 
-        $('html, body').animate({
-          scrollTop: offset
-        }, 100);
-      });
-    });
-
-
+$(document).on("click", function (event) {
+  if ($(window).width() < 800) {
+    const navBar = $("nav .nav-links");
+    const burgerIcon = $(".burger");
+    // Check if the clicked element is not inside the navBar or burgerIcon
+    if (
+      !navBar.is(event.target) &&
+      !burgerIcon.is(event.target) &&
+      navBar.has(event.target).length === 0 &&
+      burgerIcon.has(event.target).length === 0
+    ) {
+      navBar.removeAttr("id");
+      burgerIcon.removeClass("cross");
+    }
+  }
+});
